8019dd15ebeb07d707a5e6a29de0e56a85705c23
!dz <104043390+dhnnz@users.noreply.github.com>
2022-05-06 14:11:00 +0700

    Create Loader.php

---
+++ src/dz/Loader.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace dz;
+
+class Loader extends \pocketmine\plugin\PluginBase{
+    
+    public function onLoad(): void{
+    //coming soon
+    }
+    
+    public function onEnable(): void{
+    //coming soon
+    }
+}
+?>
